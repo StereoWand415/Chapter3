95d603da79d5931df48dfd0bd3837b3b1ff90f86
StereoWand415 <70332520+StereoWand415@users.noreply.github.com>
2021-12-06 10:36:05 -0600

    Add files via upload

---
+++ SpawnManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefabs;
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerController playerControllerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle ()
+    {
+        if (!playerControllerScript.gameOver)
+        {
+            int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+          Vector3 spawnPos = new Vector3(25, obstaclePrefabs[obstacleIndex].transform.position.y, 0);
+          Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
+        }
+     
+    }
+}
